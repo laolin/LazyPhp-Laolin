cdceb14bc924a36796f424b87fc6fe4b8510a5c6
LaoLin <hi@laolin.com>
2013-05-31 22:53:49 +0800

    footer

--- linjianping/view/web/footer.inc.php
+++ linjianping/view/web/footer.inc.php
@@ -1,4 +1,10 @@
 <hr/>
 <footer>
-<p>&copy; <a href='http://laolin.com'>LaoLin</a> 2013</p>
+<p>&copy; 
+
+<?php
+echo $_SERVER['HTTP_HOST']=='laolin.com'?
+"<a href='http://laolin.com'>LaoLin</a>":
+"<a href='http://linjianping.com'>林建萍</a>";
+ echo date('Y')?></p>
 </footer>
